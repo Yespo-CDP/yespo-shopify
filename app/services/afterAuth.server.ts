d776394a821880f6db0f4a839220f0e9925432e2
Maksim Bahmackij <maxim.bahmackij@devit.group>
2025-02-21 12:49:48 +0200

    add afterAuth service

---
+++ app/services/afterAuth.server.ts
@@ -0,0 +1,61 @@
+import type { Session } from "@shopify/shopify-api";
+
+import type { ShopDataResponse } from "~/@types/shop";
+import { shopRepository } from "~/repositories/repositories.server";
+import shopify from "~/shopify.server";
+
+const afterAuth = async ({
+  session,
+  admin,
+}: {
+  session: Session;
+  admin: any;
+}): Promise<void> => {
+  const response = await admin.graphql(`
+    #graphql
+    query {
+      shop {
+        id
+        myshopifyDomain
+        contactEmail
+        email
+        name
+        url
+        primaryDomain {
+          id
+          host
+        }
+      }
+    }
+  `);
+
+  const responseParse = await response.json();
+  const shopData = responseParse?.data as ShopDataResponse;
+  const shop = shopData?.shop;
+
+  if (!shop) {
+    throw new Error("Shop not found");
+  }
+
+  const shopObject = await shopRepository.getShop(session?.shop);
+
+  if (shopObject) {
+    await shopRepository.updateShop(session.shop, {
+      name: shop.name,
+      email: shop.email,
+      domain: shop?.primaryDomain?.host,
+    });
+  } else {
+    await shopRepository.createShop({
+      shopUrl: shop.myshopifyDomain,
+      shopId: shop.id,
+      name: shop.name,
+      email: shop.email,
+      domain: shop?.primaryDomain?.host,
+    });
+  }
+
+  shopify.registerWebhooks({ session });
+};
+
+export default afterAuth;
--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -5,7 +5,9 @@ import {
   shopifyApp,
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import prisma from "./db.server";
+
+import prisma from "~/db.server";
+import afterAuth from "~/services/afterAuth.server";
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
@@ -16,6 +18,9 @@ const shopify = shopifyApp({
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
+  hooks: {
+    afterAuth,
+  },
   future: {
     unstable_newEmbeddedAuthStrategy: true,
     removeRest: true,
