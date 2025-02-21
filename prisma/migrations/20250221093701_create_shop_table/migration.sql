45a2d101d23949da4d20898abc43f20d1d1d937d
Maksim Bahmackij <maxim.bahmackij@devit.group>
2025-02-21 12:45:55 +0200

    add shop table

---
+++ prisma/migrations/20250221093701_create_shop_table/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "Shop" (
+    "id" SERIAL NOT NULL,
+    "shopId" TEXT NOT NULL,
+    "shopUrl" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "domain" TEXT NOT NULL DEFAULT '',
+    "apiKey" TEXT,
+    "isScriptActive" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Shop_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Shop_shopId_key" ON "Shop"("shopId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Shop_shopUrl_key" ON "Shop"("shopUrl");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Shop_domain_key" ON "Shop"("domain");
