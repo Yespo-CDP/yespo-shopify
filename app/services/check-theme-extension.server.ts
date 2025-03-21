21d74dbc8d7fef012545809cc4a97a72943418c9
Maksim Bahmackij <maxim.bahmackij@devit.group>
2025-03-21 18:02:41 +0200

    refactor code

--- app/services/check-theme-extension.server.ts
+++ app/services/check-theme-extension.server.ts
@@ -2,37 +2,45 @@ import commentJSON from "comment-json";
 
 import getThemes from "~/shopify/queries/get-themes";
 
+const GENERAL_SCRIPT_HANDLE =
+  process.env.GENERAL_SCRIPT_HANDLE ?? "yespo-script";
+
 async function checkThemeExtensionService({
   admin,
 }: {
   admin: any;
 }): Promise<boolean> {
-  const themes = await getThemes({ admin });
-  const publishedTheme = themes?.find((theme) => theme.role === "MAIN");
+  try {
+    const themes = await getThemes({ admin });
+    const publishedTheme = themes?.find((theme) => theme.role === "MAIN");
 
-  if (!publishedTheme) {
+    if (!publishedTheme) {
+      return false;
+    }
+
+    const config = publishedTheme.files.nodes.find(
+      (file) => file.filename === "config/settings_data.json",
+    );
+
+    if (!config) {
+      return false;
+    }
+
+    const configParse = commentJSON.parse(config.body.content) as unknown as {
+      current?: { blocks: { type: string; disabled: boolean }[] };
+    };
+    const appBlocks = configParse?.current?.blocks
+      ? Object.values(configParse?.current.blocks)
+      : [];
+    const antlaBlock = appBlocks.find(({ type }) =>
+      type.includes(GENERAL_SCRIPT_HANDLE),
+    );
+
+    return antlaBlock ? !antlaBlock?.disabled : false;
+  } catch (error) {
+    console.error(error);
     return false;
   }
-
-  const config = publishedTheme.files.nodes.find(
-    (file) => file.filename === "config/settings_data.json",
-  );
-
-  if (!config) {
-    return false;
-  }
-
-  const configParse = commentJSON.parse(config.body.content) as unknown as {
-    current?: { blocks: { type: string; disabled: boolean }[] };
-  };
-  const appBlocks = configParse?.current?.blocks
-    ? Object.values(configParse?.current.blocks)
-    : [];
-  const antlaBlock = appBlocks.find(({ type }) =>
-    type.includes(process.env.GENERAL_SCRIPT_HANDLE ?? "yespo-script"),
-  );
-
-  return antlaBlock ? !antlaBlock?.disabled : false;
 }
 
 export default checkThemeExtensionService;
