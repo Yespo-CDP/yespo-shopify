c4e7c03335176e06f085b3109ce1271dd1588ad7
Margarita Saman <margarita.saman@devit.group>
2025-06-05 17:00:13 +0300

    Fix

--- app/services/delete-contact.service.ts
+++ app/services/delete-contact.service.ts
@@ -1,13 +1,9 @@
 import {deleteContact} from "~/api/delete-contact";
 
 export const deleteContactService = async (externalCustomerId: string, apiKey: string, erase: boolean) => {
-  try {
-    await deleteContact({
-      apiKey,
-      externalCustomerId,
-      erase
-    })
-  } catch (error) {
-    console.error(error);
-  }
+  await deleteContact({
+    apiKey,
+    externalCustomerId,
+    erase
+  })
 }
