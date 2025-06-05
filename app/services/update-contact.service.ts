7b5f4032677e90149a86f8cc642b6e93d591fa05
Margarita Saman <margarita.saman@devit.group>
2025-06-05 17:00:43 +0300

    Fix

--- app/services/update-contact.service.ts
+++ app/services/update-contact.service.ts
@@ -1,50 +1,46 @@
 import {updateContact} from "~/api/update-contact";
 
 export const updateContactService = async (payload: any, apiKey: string) => {
-  try {
-    const channels = []
-    let address = {}
+  const channels = []
+  let address = {}
 
-    if (payload.email) {
-      channels.push({
-        type: 'email',
-        value: payload.email,
-      })
-    }
-
-    if (payload.phone) {
-      channels.push({
-        type: 'sms',
-        value: payload.phone,
-      })
-    } else if (payload.default_address.phone) {
-      channels.push({
-        type: 'sms',
-        value: payload.default_address.phone,
-      })
-    }
-
-    if (payload.default_address) {
-      address = {
-        town: payload.default_address.city || '',
-        address: payload.default_address.address1 || '',
-        postcode: payload.default_address.zip || '',
-      }
-    }
-
-    const contactData = {
-      firstName: payload.first_name || '',
-      lastName: payload.last_name || '',
-      channels,
-      externalCustomerId: payload.id.toString(),
-      address: address
-    }
-
-    await updateContact({
-      apiKey,
-      contactData
+  if (payload.email) {
+    channels.push({
+      type: 'email',
+      value: payload.email,
     })
-  } catch (error) {
-    console.error(error);
   }
+
+  if (payload.phone) {
+    channels.push({
+      type: 'sms',
+      value: payload.phone,
+    })
+  } else if (payload.default_address.phone) {
+    channels.push({
+      type: 'sms',
+      value: payload.default_address.phone,
+    })
+  }
+
+  if (payload.default_address) {
+    address = {
+      town: payload.default_address.city || '',
+      address: payload.default_address.address1 || '',
+      postcode: payload.default_address.zip || '',
+    }
+  }
+
+  const contactData = {
+    firstName: payload.first_name || '',
+    lastName: payload.last_name || '',
+    channels,
+    externalCustomerId: payload.id.toString(),
+    address: address
+  }
+
+  await updateContact({
+    apiKey,
+    contactData
+  })
 }
