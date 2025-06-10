58f03ed2d84cf6c66bfe3a3ec6862a813750d76f
Margarita Saman <margarita.saman@devit.group>
2025-06-10 17:10:52 +0300

    Updated

--- app/repositories/repositories.server.ts
+++ app/repositories/repositories.server.ts
@@ -1,6 +1,6 @@
 import ShopRepositoryImpl from "~/repositories/shop/shopRepositoryImpl.server";
-import database from "~/db.server";
 import CustomerDataRepositoryImpl from "~/repositories/customerData/customerDataRepositoryImpl.server";
+import database from "~/db.server";
 
 const shopRepository = new ShopRepositoryImpl(database);
 const customerDataRepository = new CustomerDataRepositoryImpl(database)
