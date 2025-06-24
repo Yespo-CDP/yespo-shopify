ec08f62097a8b73147be3ca96b2696549344d739
Margarita Saman <margarita.saman@devit.group>
2025-06-24 11:36:54 +0300

    Added sending a CustomerData event

--- extensions/yespo-extension/assets/web-tracking.js
+++ extensions/yespo-extension/assets/web-tracking.js
@@ -1,26 +1,41 @@
-function page404(eS, pageTemplate) {
+function sendPage404Event(eS, pageTemplate) {
   if (pageTemplate === '404') {
     console.log('404 PAGE')
     eS('sendEvent', 'NotFound');
   }
 }
 
-function mainPage(eS, pageTemplate) {
+function sendMainPageEvent(eS, pageTemplate) {
   if (pageTemplate === 'index') {
     console.log('MAIN PAGE')
     eS('sendEvent', 'MainPage');
   }
 }
 
-function productPage(eS, pageTemplate, product) {
+function sendProductPageEvent(eS, pageTemplate, product) {
   if (pageTemplate === 'product' && !!product) {
     console.log('PRODUCT PAGE')
-    window.eS('sendEvent', 'ProductPage', {
+    eS('sendEvent', 'ProductPage', {
       'ProductPage': {
         'productKey': product.id.toString(),
         'price': product.price.toString(),
         'isInStock': product.available ? 1 : 0
       }});
+  }
+}
+
+function sendCustomerEvent(eS, customer) {
+  if (customer) {
+    console.log('CUSTOMER DATA')
+    eS('sendEvent', 'CustomerData', {
+      'CustomerData': {
+        'externalCustomerId': customer.id.toString(),
+        'user_email': customer.email,
+        'user_name': `${customer.firstName} ${customer.lastName}`,
+        'user_phone': customer.phone,
+        'user_city': customer.city,
+      }
+    })
   }
 }
 
@@ -32,11 +47,14 @@ function processEvents (d,s) {
   var data = JSON.parse(d.getElementById('data').textContent);
   if (!data) return
 
-  page404(window.eS, data.pageTemplate);
-  mainPage(window.eS, data.pageTemplate)
-  productPage(window.eS, data.pageTemplate, data.product)
+  console.log('data', data)
+
+  sendPage404Event(window.eS, data.pageTemplate);
+  sendMainPageEvent(window.eS, data.pageTemplate)
+  sendProductPageEvent(window.eS, data.pageTemplate, data.product)
+  sendCustomerEvent(window.eS, data.customer)
 }
 (function(d,s){
   console.log("EVENT SCRIPT STARTED")
-  processEvents(document, 'script')
+  processEvents(d, s)
 })(document, 'script');
