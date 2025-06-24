91fcb3904a795b2285c859b841f5ca771d4e4fa6
Margarita Saman <margarita.saman@devit.group>
2025-06-24 10:37:35 +0300

    Added sending a ProductPage event

--- extensions/yespo-extension/assets/web-tracking.js
+++ extensions/yespo-extension/assets/web-tracking.js
@@ -1,24 +1,42 @@
-(function(d,s){
-  console.log("EVENT SCRIPT STARTED")
+function page404(eS, pageTemplate) {
+  if (pageTemplate === '404') {
+    console.log('404 PAGE')
+    eS('sendEvent', 'NotFound');
+  }
+}
 
+function mainPage(eS, pageTemplate) {
+  if (pageTemplate === 'index') {
+    console.log('MAIN PAGE')
+    eS('sendEvent', 'MainPage');
+  }
+}
+
+function productPage(eS, pageTemplate, product) {
+  if (pageTemplate === 'product' && !!product) {
+    console.log('PRODUCT PAGE')
+    window.eS('sendEvent', 'ProductPage', {
+      'ProductPage': {
+        'productKey': product.id.toString(),
+        'price': product.price.toString(),
+        'isInStock': product.available ? 1 : 0
+      }});
+  }
+}
+
+function processEvents (d,s) {
   window.eS = window.eS || function () {
     (window.eS.q = window.eS.q || []).push(arguments);
   };
 
-  var data = JSON.parse(document.getElementById('data').textContent);
+  var data = JSON.parse(d.getElementById('data').textContent);
+  if (!data) return
 
-  if (!data) {
-    return
-  }
-
-  console.log('DATA', data)
-  console.log('CURRENT PAGE', data.pageTemplate)
-
-  if (data.pageTemplate === '404') {
-    window.eS('sendEvent', 'NotFound');
-  }
-
-  if (data.pageTemplate === 'index') {
-    window.eS('sendEvent', 'MainPage');
-  }
+  page404(window.eS, data.pageTemplate);
+  mainPage(window.eS, data.pageTemplate)
+  productPage(window.eS, data.pageTemplate, data.product)
+}
+(function(d,s){
+  console.log("EVENT SCRIPT STARTED")
+  processEvents(document, 'script')
 })(document, 'script');
