898656a42bdced8c62b8efe08aed63487ae090ee
Maksim Bahmackij <maxim.bahmackij@devit.group>
2025-08-01 17:14:33 +0300

    add contact sync option validation to webhook

--- app/routes/webhooks.app.customers.tsx
+++ app/routes/webhooks.app.customers.tsx
@@ -1,8 +1,8 @@
-import type { ActionFunctionArgs } from '@remix-run/node'
-import { authenticate } from '../shopify.server'
-import {shopRepository} from "~/repositories/repositories.server";
-import {createContactService} from "~/services/create-contact.server";
-import {updateContactService} from "~/services/update-contact.service";
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { authenticate } from "../shopify.server";
+import { shopRepository } from "~/repositories/repositories.server";
+import { createContactService } from "~/services/create-contact.server";
+import { updateContactService } from "~/services/update-contact.service";
 
 /**
  * Action handler for processing incoming Shopify webhooks.
@@ -16,39 +16,42 @@ import {updateContactService} from "~/services/update-contact.service";
  *
  * If the session is not exist responds with HTTP 200.
  * If the shop is not found or does not have an API key, responds with HTTP 200.
+ * If sync contacts disabled responds with HTTP 200.
  * If the webhook topic is unhandled, responds with HTTP 400 and logs a warning.
  *
  * @param {ActionFunctionArgs} args - The arguments containing the request.
  * @returns {Promise<Response>} An HTTP response indicating success or failure.
  */
-export const action = async ({request}: ActionFunctionArgs) => {
+export const action = async ({ request }: ActionFunctionArgs) => {
   const { topic, session, payload, webhookId } =
-    await authenticate.webhook(request)
+    await authenticate.webhook(request);
 
   if (!session) {
-    return new Response("Success", {status: 200});
+    return new Response("Success", { status: 200 });
   }
-  console.log(`Received ${topic} webhook for ${session.shop} webhookId ${webhookId}`);
+  console.log(
+    `Received ${topic} webhook for ${session.shop} webhookId ${webhookId}`,
+  );
 
   const shop = await shopRepository.getShop(session.shop);
 
-  if (!shop || !shop?.apiKey) {
-    return new Response("Success", {status: 200});
+  if (!shop || !shop?.apiKey || !shop?.isContactSyncEnabled) {
+    return new Response("Success", { status: 200 });
   }
 
   switch (topic) {
     case "CUSTOMERS_CREATE":
-      await createContactService(payload, shop.apiKey)
+      await createContactService(payload, shop.apiKey);
       break;
 
     case "CUSTOMERS_UPDATE":
-      await updateContactService(payload, shop.apiKey)
+      await updateContactService(payload, shop.apiKey);
       break;
 
     default:
       console.warn(`❌ Unhandled webhook topic: ${topic}`);
-      return new Response(`Unhandled webhook topic: ${topic}`, {status: 200});
+      return new Response(`Unhandled webhook topic: ${topic}`, { status: 200 });
   }
 
-  return new Response("Success", {status: 200});
+  return new Response("Success", { status: 200 });
 };
