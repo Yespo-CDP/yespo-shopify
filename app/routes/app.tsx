8eeff68cd5f7a516825867e7e26ae7f4c97fcd9a
Margarita Saman <margarita.saman@devit.group>
2025-11-26 11:42:46 +0200

    Remove unnecessary nav link

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,9 +1,7 @@
-
 import type { HeadersFunction, LoaderFunctionArgs } from "react-router";
 import {  Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
-
 
 import { authenticate } from "../shopify.server";
 
@@ -33,9 +31,6 @@ export default function App() {
 
   return (
     <AppProvider embedded apiKey={apiKey}>
-      <s-app-nav>
-        <s-link href="/app" >Home</s-link>
-      </s-app-nav>
       <Outlet />
     </AppProvider>
   );
