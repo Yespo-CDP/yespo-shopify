ece13d4bf6ea8f38d2cfb12d63c78d1fa2507f25
Olena Shevchuk <olenashevchuk0@gmail.com>
2026-09-01 08:46:50 +0300

    Rename web push script log events to GET_WEB_PUSH_SCRIPT_*

--- app/utils/fetchWithErrorHandling.ts
+++ app/utils/fetchWithErrorHandling.ts
@@ -86,17 +86,10 @@ export async function fetchWithErrorHandling(
 ) {
   for (let attempt = 0; ; attempt++) {
     try {
-      console.log("***************");
-      console.log("url", url);
-      console.log("options", options);
-
       const response = await fetch(url, options);
 
       const rawResponse = await response.text();
       let responseData: any = rawResponse;
-      console.log("***************");
-      console.log("rawResponse", rawResponse);
-      console.log("response.status", response.status);
 
       try {
         responseData = JSON.parse(rawResponse);
